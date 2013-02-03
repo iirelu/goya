497106a32a41edded946577bbcbd4e685dd198a3
iirelu <auto63436382@hush.ai>
2013-02-03 21:56:22 +0000

    Working on new loading system

--- load.js
+++ load.js
@@ -1,11 +1,54 @@
 (function (global) {
   "use strict";
-  var Load = function (callback, resources) {
-    if (arguments.length === 0) {
-      throw new Error("Not enough arguments to Load()");
-    }
-    global.console.log("hey! im still here!");
-  }
+  var
+    Load = function (resources, callback) {
+      debugger;
+      if (arguments.length === 0) {
+        throw new Error("Not enough arguments to Load()");
+      }
+      currentLoaded = 0;
+      toLoad = 0;
+      currentCallbacks = callback;
+      currentElements = parseResources(resources);
+    },
+    parseResources = function (resources) {
+      var parsed = {},
+        name, res, currentRes;
+
+      for (name in resources) {
+        if (resources.hasOwnProperty(name)) {
+          parsed[name] = [];
+          for (res = 0; res < resources[name].length; res += 1) {
+            if (!loadedElements[res]) {
+              parsed[name][res] = createElement(name, resources[name][res]);
+              loadedElements[res] = parsed[name][res];
+            } else {
+              parsed[name][res] = loadedElements[res];
+            }
+            toLoad += 1;
+          }
+        }
+      }
+      return parsed;
+    },
+    createElement = function (name, src) {
+      var element = global.document.createElement(name);
+      element.src = src;
+      element.addEventListener("load", loaded, false);
+      element.addEventListener("error", currentCallbacks.failed, false);
+      return element;
+    },
+    loaded = function () {
+      currentLoaded += 1;
+      if (toLoad === currentLoaded) {
+        currentCallbacks.success(currentElements);
+      }
+    },
+    loadedElements = {},
+    currentElements,
+    toLoad,
+    currentLoaded,
+    currentCallbacks;
 
   global.Load = Load;
 }(this));
